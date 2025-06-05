6bf0cbc911c064ae70bb6521da3787910ca754ba
SamCosner <scosner@iu.edu>
2025-06-05 16:07:56 -0400

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import solidPlugin from 'vite-plugin-solid';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [tailwindcss(),solidPlugin()],
+  plugins: [tailwindcss(), solidPlugin()],
   server: {
     port: 3000,
+    allowedHosts: ['devserver-preview--acrex.netlify.app']
   },
   build: {
     target: 'esnext',
